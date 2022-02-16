2b53ded52ef3921af1b99c10a2e9c318613bf668
arty <artysha.rylit12@gmail.com>
2022-02-16 11:25:50 +0300

    change indent space

--- src/api/geoLocation/geoLocation.ts
+++ src/api/geoLocation/geoLocation.ts
@@ -2,27 +2,27 @@ import axios from "axios";
 import { GEOLOCATION_DEFINE_URL, GET_CITIES_URL } from './geoLocation.model';
 
 export const getUserCity = async (coords: any) => {
-    const options = {
-      method: 'POST',
-      mode: 'cors',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Token ${process.env.REACT_APP_ENV_DADATA_TOKEN}`,
-      },
-    };
-    const optionsWithGeoData = {
-      ...options,
-      body: JSON.stringify(coords),
-    };
+  const options = {
+    method: 'POST',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Token ${process.env.REACT_APP_ENV_DADATA_TOKEN}`,
+    },
+  };
+  const optionsWithGeoData = {
+    ...options,
+    body: JSON.stringify(coords),
+  };
 
-    const data = JSON.parse(optionsWithGeoData.body);
-    const config = {
-      headers: {
-        Authorization: optionsWithGeoData.headers.Authorization,
-      },
-    };
-    
-    return axios.post(GEOLOCATION_DEFINE_URL, data, config).then(res => res.data)
+  const data = JSON.parse(optionsWithGeoData.body);
+  const config = {
+    headers: {
+      Authorization: optionsWithGeoData.headers.Authorization,
+    },
+  };
+  
+  return axios.post(GEOLOCATION_DEFINE_URL, data, config).then(res => res.data)
 };
 
 
@@ -33,9 +33,9 @@ export const getCities = (city: string) => {
       method: "POST",
       mode: "cors",
       headers: {
-          "Content-Type": "application/json",
-          "Accept": "application/json",
-          "Authorization": "Token " + process.env.REACT_APP_ENV_DADATA_TOKEN,
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "Authorization": "Token " + process.env.REACT_APP_ENV_DADATA_TOKEN,
       },
       body: JSON.stringify({query: query})
   }
