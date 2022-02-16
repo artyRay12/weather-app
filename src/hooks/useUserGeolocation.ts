2c4b7c50998ba1753288f7b67b88426fe473c9cc
arty <artysha.rylit12@gmail.com>
2022-02-16 15:15:20 +0300

    add type for hook prop

--- src/hooks/useUserGeolocation.ts
+++ src/hooks/useUserGeolocation.ts
@@ -1,18 +1,19 @@
 import { changeGeolocationAccess, getPosition } from "../redux/geoLocationSlice";
 import { useDispatch } from 'react-redux';
+import { Dispatch, SetStateAction } from "react";
 
-export const useUserGeolocation = (callback?: any) => {
+export const useUserGeolocation = (showGeolocationWarining?: Dispatch<SetStateAction<boolean>>) => {
   const dispatch = useDispatch();
   const saveCoods = (result: any) => {
     const { coords: { latitude: lat, longitude: lon } } = result;
     dispatch(changeGeolocationAccess(true));
     dispatch(getPosition({ lat, lon}));
-    callback && callback(false);
+    showGeolocationWarining && showGeolocationWarining(false);
   }
 
   const userGeoIsOff = () => {
     dispatch(changeGeolocationAccess(false));
-    callback && callback(true);
+    showGeolocationWarining && showGeolocationWarining(true);
   }
   return [() => window.navigator.geolocation.getCurrentPosition(saveCoods, userGeoIsOff)]
 }
